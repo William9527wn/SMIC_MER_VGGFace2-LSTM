47021f1ac17723461c15b2ce31a0cfcff2ce48cf
Keira <52691373+KeiraLalala@users.noreply.github.com>
2020-10-15 13:33:47 +1100

    Add files via upload

---
+++ functions_M.py
@@ -0,0 +1,139 @@
+import os
+import numpy as np
+from PIL import Image
+from torch.utils import data
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import Vggface2model as model
+import torchvision.transforms as transforms
+from tqdm import tqdm
+
+## ------------------------ CRNN module ---------------------- ##
+
+
+# 2D CNN encoder using VGGFace2 model pretrained
+class VggFaceEncoder(nn.Module):
+    def __init__(self, fc_hidden1=512, fc_hidden2=512, drop_p=0.3, CNN_embed_dim=300):
+        """Load the pretrained ResNet-152 and replace top fc layer."""
+        super(VggFaceEncoder, self).__init__()
+
+        self.fc_hidden1, self.fc_hidden2 = fc_hidden1, fc_hidden2
+        self.drop_p = drop_p
+        self.resnet = model.resnet50_128(weights_path='Vggface2weight.pth') 
+        
+        self.fc1 = nn.Linear(2048, fc_hidden1)
+        self.bn1 = nn.BatchNorm1d(fc_hidden1, momentum=0.01)
+        self.fc2 = nn.Linear(fc_hidden1, fc_hidden2)
+        self.bn2 = nn.BatchNorm1d(fc_hidden2, momentum=0.01)
+        self.fc3 = nn.Linear(fc_hidden2, CNN_embed_dim)
+        
+    def getgradcam(self, layer, data):
+        data = self.resnet.grad_layer(layer, data)        
+        return data
+    
+    def forward(self, x_3d):
+        cnn_embed_seq = []
+       
+        for t in range(x_3d.size(1)):            
+            # ResNet CNN
+            with torch.no_grad():  
+                x = self.resnet(x_3d[:, t, :, :, :])  # ResNet
+                x = x.view(x.size(0), -1)             # flatten output of conv
+            
+            # FC layers
+            x = self.fc1(x)
+            x = self.bn1(x)
+            x = F.relu(x)
+            x = self.bn2(self.fc2(x))
+            x = F.relu(x)
+            x = F.dropout(x, p=self.drop_p, training=self.training)
+            x = self.fc3(x)
+
+            cnn_embed_seq.append(x)
+
+        # swap time and sample dim such that (sample dim, time dim, CNN latent dim)
+        cnn_embed_seq = torch.stack(cnn_embed_seq, dim=0).transpose_(0, 1)
+        # cnn_embed_seq: shape=(batch, time_step, input_size)
+
+        return cnn_embed_seq
+    
+#Bi-directional LSTM
+class DecoderBRNN(nn.Module):
+    def __init__(self, CNN_embed_dim=300, h_RNN_layers=3, h_RNN=256, h_FC_dim=128, drop_p=0.3, num_classes=50):
+        super(DecoderBRNN, self).__init__()
+
+        self.RNN_input_size = CNN_embed_dim
+        self.h_RNN_layers = h_RNN_layers   # RNN hidden layers
+        self.h_RNN = h_RNN                 # RNN hidden nodes
+        self.h_FC_dim = h_FC_dim
+        self.drop_p = drop_p
+        self.num_classes = num_classes
+
+        self.LSTM = nn.LSTM(
+            input_size=self.RNN_input_size,
+            hidden_size=self.h_RNN,        
+            num_layers=h_RNN_layers,   
+            bidirectional=True,
+            batch_first=True,       # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+        )
+        
+        self.fc1 = nn.Linear(self.h_RNN*2, self.h_FC_dim)
+        self.fc2 = nn.Linear(self.h_FC_dim, self.num_classes)
+
+    def forward(self, x_RNN):
+        
+        self.LSTM.flatten_parameters()
+        RNN_out, (h_n, h_c) = self.LSTM(x_RNN, None)  
+        """ h_n shape (n_layers, batch, hidden_size), h_c shape (n_layers, batch, hidden_size) """ 
+        """ None represents zero initial hidden state. RNN_out has shape=(batch, time_step, output_size) """
+
+        # FC layers
+        x = self.fc1(RNN_out[:, -1, :])   # choose RNN_out at the last time step
+        x = F.relu(x)
+        x = F.dropout(x, p=self.drop_p, training=self.training)
+        x = self.fc2(x)
+
+        return x
+
+## ---------------------- end of CRNN module ---------------------- ##
+
+#3 Layers-LSTM
+class DecoderRNN(nn.Module):
+    def __init__(self, CNN_embed_dim=300, h_RNN_layers=3, h_RNN=256, h_FC_dim=128, drop_p=0.3, num_classes=50):
+        super(DecoderRNN, self).__init__()
+
+        self.RNN_input_size = CNN_embed_dim
+        self.h_RNN_layers = h_RNN_layers   # RNN hidden layers
+        self.h_RNN = h_RNN                 # RNN hidden nodes
+        self.h_FC_dim = h_FC_dim
+        self.drop_p = drop_p
+        self.num_classes = num_classes
+
+        self.LSTM = nn.LSTM(
+            input_size=self.RNN_input_size,
+            hidden_size=self.h_RNN,        
+            num_layers=h_RNN_layers,
+            batch_first=True,       # input & output will has batch size as 1s dimension. e.g. (batch, time_step, input_size)
+        )
+        
+        self.fc1 = nn.Linear(self.h_RNN, self.h_FC_dim)
+        self.fc2 = nn.Linear(self.h_FC_dim, self.num_classes)
+
+    def forward(self, x_RNN):
+        
+        self.LSTM.flatten_parameters()
+        RNN_out, (h_n, h_c) = self.LSTM(x_RNN, None)  
+        """ h_n shape (n_layers, batch, hidden_size), h_c shape (n_layers, batch, hidden_size) """ 
+        """ None represents zero initial hidden state. RNN_out has shape=(batch, time_step, output_size) """
+
+        # FC layers
+        x = self.fc1(RNN_out[:, -1, :])   # choose RNN_out at the last time step
+        x = F.relu(x)
+        x = F.dropout(x, p=self.drop_p, training=self.training)
+        x = self.fc2(x)
+
+        return x
+
+## ---------------------- end of CRNN module ---------------------- ##
+
